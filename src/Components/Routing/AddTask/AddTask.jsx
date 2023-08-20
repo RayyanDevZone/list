f7e0266ada23dc0a4aff24ba77f5c18f4b03cf95
RayyanDevZone <rayyansyed369@gmail.com>
2023-08-20 10:37:21 +0530

    third commit

--- src/Components/Routing/AddTask/AddTask.jsx
+++ src/Components/Routing/AddTask/AddTask.jsx
@@ -1,56 +1,3 @@
-// import React, { useState } from 'react';
-// import './AddTask.scss';
-// import { Link } from 'react-router-dom';
-// import Cross from '../../../images/close 1.svg';
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// const AddTask = () => {
-//   const [selectedCategory, setSelectedCategory] = useState('');
-//   const [taskText, setTaskText] = useState('');
-
-//   const isFormValid = selectedCategory !== '' && taskText.trim() !== '';
-
-//   return (
-//     <div className='add'>
-//       <div className='adding'>
-//         <Link to="/all-tasks">
-//           <img id='close' src={Cross} alt='close' />
-//         </Link>
-//         <h1 id='ad'>ADD TASKS</h1>
-//       </div>
-//       <div className='background-blue'>
-//         <div className='category'>
-//           <label htmlFor='categorySelect'>Select a category</label>
-//           <select required id='categorySelect' onChange={(e) => setSelectedCategory(e.target.value)}>
-//             <option value='empty'></option>
-//             <option value='work'>Work</option>
-//             <option value='study'>Study</option>
-//             <option value='travel'>Travel</option>
-//             <option value='home'>Home</option>
-//             <option value='shopping'>Shopping</option>
-//           </select>
-
-//           <label htmlFor='taskInput'>Type a task here</label>
-//           <textarea
-//           required
-//             id='taskInput'
-//             rows='4'
-//             value={taskText}
-//             onChange={(e) => setTaskText(e.target.value)}
-//           ></textarea>
-
-//           <button id='btn' disabled={!isFormValid}>
-//             Create
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AddTask;
-
 import React, { useState } from "react";
 import "./AddTask.scss";
 import { Link } from "react-router-dom";
@@ -66,22 +13,19 @@ const AddTask = () => {
 
   const handleCreateTask = () => {
     if (isFormValid) {
-      // Add your task creation logic here
-
-      // Display toast message for successful task addition
+     
       toast.success("Task Added Successfully", {
         position: toast.POSITION.BOTTOM_CENTER,
         autoClose: 3000,
         style: {
-          background: 'green', // Set the background color
+          background: 'green', 
           color: 'white',
           borderRadius:'30px',
           boxShadow:" 0px 4px 4px 0px #00000040" ,
-          padding:"7px 22px"// Set the text color
+          padding:"7px 22px"
         },
       });
 
-      // Clear the input fields after successful task addition
       setSelectedCategory("");
       setTaskText("");
     }
